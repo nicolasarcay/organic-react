b96976ed7ec4c4ec7a4a37bc4202f8d0330e5e4b
nicolasarcay <hola@nicolasarcay.com>
2021-06-19 11:01:04 -0300

    error import ProductDetail

---
+++ src/components/products-layout/HeaderProduct.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Container, Row, Col } from "react-bootstrap";
+import ButtonCards from "../buttons/ButtonCards";
+import CounterCard from "../counters/CounterCard";
+
+function HeaderProduct() {
+    return (
+        <div>
+            <div className="mobile-bg d-block d-md-none ">
+
+            </div>
+            <div className="desktop-bg d-none d-md-block">
+
+            </div>
+        </div>
+    )
+}
+
+export default HeaderProduct
--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import HeaderProduct from '../components/products-layout/HeaderProduct'
+import ContentProduct from '../components/products-layout/ContentProduct'
+import RealtedProduct from '../components/products-layout/RealtedProduct'
 
 function ProductDetail() {
     return (
         <div>
-            Producto individual
+            <HeaderProduct />
+            <ContentProduct />
+            <RealtedProduct />
         </div>
     )
 }
